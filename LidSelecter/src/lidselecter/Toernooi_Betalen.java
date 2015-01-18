40435c963b73716fc9d38578a6ef6ba8de0dad55
LorenzoVoermans <lorenzovoermans@hotmail.com>
2015-01-18 18:30:57 +0100

    toernooi betalen v1.0

--- LidSelecter/src/lidselecter/Toernooi_Betalen.java
+++ LidSelecter/src/lidselecter/Toernooi_Betalen.java
@@ -17,7 +17,13 @@ import javax.swing.table.DefaultTableModel;
  */
 public class Toernooi_Betalen extends javax.swing.JFrame {
 
-    private final DefaultTableModel table = new DefaultTableModel();
+    private final DefaultTableModel table = new DefaultTableModel() {
+
+        @Override
+        public boolean isCellEditable(int row, int column) {
+            return false;
+        }
+    };
     DefaultListModel jListModel = new DefaultListModel();
     int toernooideelnemernummer = 0;
     boolean heeftbetaald = false;
@@ -28,29 +34,31 @@ public class Toernooi_Betalen extends javax.swing.JFrame {
         setLocationRelativeTo(null);
         IngeschrevenDeelnemer.setModel(jListModel);
         ToernooiOphalen.setModel(table);
-        String[] Kolomnaam = {"Toernooi id", "Datum", "Plaats code", "Max spelers"};
+        String[] Kolomnaam = {"Id", "Datum", "Max spelers", "Betaald", "Niet Betaald"};
         table.setColumnIdentifiers(Kolomnaam);
         table.setRowCount(0);
-        table.setColumnCount(4);
+        table.setColumnCount(5);
         toernooiVullen();
-        jLabel1.setText("");
+        jLabel1.setText("Speler:");
     }
 
     private void toernooiVullen() {
-        // TODO add your handling code here:
+
         Sql_connect.doConnect();
-        // declareer de variable voor in de rs
+
         String id;
         String naam;
-        String plaats;
+        //String plaats;
         String Max_inschrijvingen_T;
+        int Betaald;
+        int Niet_Betaald;
 
 
         try {
 
             Sql_connect.doConnect();
 
-            String prepSqlStatement = "select * from toernooi;";
+            String prepSqlStatement = "select T.Id_toernooi,T.Naam, T.Datum,T.Max_inschrijvingen_T, count(TD.Id_persoon) - sum(TD.Isbetaald) as nietBetaald, sum(TD.Isbetaald) as betaald from toernooi T join toernooideelnemer TD on TD.Id_toernooi = T.Id_toernooi group by T.Id_toernooi;";
             PreparedStatement stat = Sql_connect.getConnection().prepareStatement(prepSqlStatement);
             ResultSet result = stat.executeQuery();
 
@@ -69,15 +77,20 @@ public class Toernooi_Betalen extends javax.swing.JFrame {
 
                 id = result.getString("Id_toernooi");
                 naam = result.getString("Datum");
-                plaats = result.getString("Id_locatie");
+                //plaats = result.getString("Id_locatie");
                 Max_inschrijvingen_T = result.getString("Max_inschrijvingen_T");
+                Betaald = result.getInt("betaald");
+                Niet_Betaald = result.getInt("nietBetaald");
+
 
 
 
                 table.setValueAt(id, d, 0);
                 table.setValueAt(naam, d, 1);
-                table.setValueAt(plaats, d, 2);
-                table.setValueAt(Max_inschrijvingen_T, d, 3);
+                //table.setValueAt(plaats, d, 2);
+                table.setValueAt(Max_inschrijvingen_T, d, 2);
+                table.setValueAt(Betaald, d, 3);
+                table.setValueAt(Niet_Betaald, d, 4);
 
 
                 d++;
@@ -103,7 +116,7 @@ public class Toernooi_Betalen extends javax.swing.JFrame {
             String Table_click = ToernooiOphalen.getModel().getValueAt(row, 0).toString();
             Sql_connect.doConnect();
 
-            // statement maken
+
             String prepSqlStatement = "select p.Id_persoon,p.Voornaam,p.Achternaam,t.Isbetaald from toernooideelnemer t join persoon p on p.Id_persoon = t.Id_persoon where t.Id_toernooi = '" + Table_click + "' ;";
             PreparedStatement stat = Sql_connect.getConnection().prepareStatement(prepSqlStatement);
             ResultSet result = stat.executeQuery();
@@ -125,20 +138,21 @@ public class Toernooi_Betalen extends javax.swing.JFrame {
     }
 
     private void gegevensLijst() {
-        jLabel1.setText("");
+        jLabel1.setText("Speler:");
+        jLabel4.setText(" ");
         try {
             if (IngeschrevenDeelnemer.getSelectedValue() == null) {
             } else {
                 ModelItem selectedItem = (ModelItem) IngeschrevenDeelnemer.getSelectedValue();
                 String Table_click = Integer.toString(selectedItem.id);
 
-                String prepSqlStatement = "select p.Id_persoon,p.Voornaam,p.Achternaam,t.Isbetaald from toernooideelnemer t join persoon p on p.Id_persoon = t.Id_persoon where t.Id_persoon = '" + Table_click + "' LIMIT 1  ;";
+                String prepSqlStatement = "select p.Id_persoon,p.Voornaam,p.Achternaam,t.Isbetaald from toernooideelnemer t join persoon p on p.Id_persoon = t.Id_persoon where t.Id_persoon = '" + Table_click + "' and t.Id_Toernooi ='" + toernooicode + "' LIMIT 1  ;";
                 PreparedStatement stat = Sql_connect.getConnection().prepareStatement(prepSqlStatement);
                 ResultSet result = stat.executeQuery();
 
                 if (result.next()) {
 
-                    jLabel1.setText(result.getString("voornaam") + " " + result.getString("achternaam"));
+                    jLabel1.setText("Speler: " + result.getString("voornaam") + " " + result.getString("achternaam"));
                     int betaald = result.getInt("Isbetaald");
                     if (betaald == 1) {
                         Betalen.setSelected(true);
@@ -171,6 +185,9 @@ public class Toernooi_Betalen extends javax.swing.JFrame {
         Updaten = new javax.swing.JButton();
         jScrollPane2 = new javax.swing.JScrollPane();
         ToernooiOphalen = new javax.swing.JTable();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -186,9 +203,14 @@ public class Toernooi_Betalen extends javax.swing.JFrame {
         });
         jScrollPane1.setViewportView(IngeschrevenDeelnemer);
 
-        jLabel1.setText("jLabel1");
+        jLabel1.setText(" ");
 
         Betalen.setText("Betaald");
+        Betalen.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BetalenActionPerformed(evt);
+            }
+        });
 
         Updaten.setText("Update");
         Updaten.addActionListener(new java.awt.event.ActionListener() {
@@ -215,6 +237,13 @@ public class Toernooi_Betalen extends javax.swing.JFrame {
         });
         jScrollPane2.setViewportView(ToernooiOphalen);
 
+        jLabel2.setText("Toernooi");
+
+        jLabel3.setText("Spelers");
+
+        jLabel4.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jLabel4.setText(" ");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -225,27 +254,43 @@ public class Toernooi_Betalen extends javax.swing.JFrame {
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 140, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap())
+            .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
+                .addGap(207, 207, 207)
+                .addComponent(jLabel2)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jLabel3)
+                .addGap(66, 66, 66))
             .addGroup(layout.createSequentialGroup()
                 .addGap(19, 19, 19)
-                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 132, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
-                .addComponent(Betalen)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(Updaten)
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 191, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(Betalen)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(Updaten))
+                    .addComponent(jLabel4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addGap(39, 39, 39))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGap(8, 8, 8)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel2)
+                    .addComponent(jLabel3))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 275, Short.MAX_VALUE)
-                    .addComponent(jScrollPane1))
+                    .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 247, Short.MAX_VALUE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel1)
                     .addComponent(Betalen)
                     .addComponent(Updaten))
-                .addGap(79, 79, 79))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel4)
+                .addGap(59, 59, 59))
         );
 
         pack();
@@ -260,29 +305,36 @@ public class Toernooi_Betalen extends javax.swing.JFrame {
     }//GEN-LAST:event_IngeschrevenDeelnemerMouseClicked
 
     private void UpdatenActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_UpdatenActionPerformed
-        //if (toernooideelnemernummer != 0) {
-        System.out.println(toernooideelnemernummer +" || "+ toernooicode);
-            try {
-                String prepSqlStatement;
-                Sql_connect.doConnect();
-                if (Betalen.isSelected()) {
-                    prepSqlStatement = "UPDATE toernooideelnemer SET Isbetaald='1' where Id_toernooi = '"+toernooicode+"' and Id_persoon = '"+toernooideelnemernummer+"' ;";
-                } else {
-                    prepSqlStatement = "UPDATE toernooideelnemer SET Isbetaald='0' where Id_toernooi = '"+toernooicode+"' and Id_persoon = '"+toernooideelnemernummer+"' ;";
-                }
-                PreparedStatement stat = Sql_connect.getConnection().prepareStatement(prepSqlStatement);
-                stat.execute();//   executeUpdate();
-                
-                Betalen.setSelected(false);
-                heeftbetaald = false;
-                toernooideelnemernummer = 0;
-                jLabel1.setText("");
-            } catch (Exception e) {
-                System.out.print(e);
+
+        //System.out.println(toernooideelnemernummer +" || "+ toernooicode);
+        try {
+            String prepSqlStatement;
+            Sql_connect.doConnect();
+            if (Betalen.isSelected()) {
+                prepSqlStatement = "UPDATE toernooideelnemer SET Isbetaald='1' where Id_toernooi = '" + toernooicode + "' and Id_persoon = '" + toernooideelnemernummer + "' ;";
+            } else {
+                prepSqlStatement = "UPDATE toernooideelnemer SET Isbetaald='0' where Id_toernooi = '" + toernooicode + "' and Id_persoon = '" + toernooideelnemernummer + "' ;";
             }
+            PreparedStatement stat = Sql_connect.getConnection().prepareStatement(prepSqlStatement);
+            stat.execute();//   executeUpdate();
+
+            Betalen.setSelected(false);
+            heeftbetaald = false;
+            toernooideelnemernummer = 0;
+            jLabel1.setText("Speler:");
+            jLabel4.setText("Update succesvol!");
+            toernooiVullen();
+        } catch (Exception e) {
+            jLabel4.setText("Er is wat mis gegaan probeer het opnieuw");
+
+        }
         //}
 
     }//GEN-LAST:event_UpdatenActionPerformed
+
+    private void BetalenActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BetalenActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_BetalenActionPerformed
 
     /**
      * @param args the command line arguments
@@ -326,6 +378,9 @@ public class Toernooi_Betalen extends javax.swing.JFrame {
     private javax.swing.JTable ToernooiOphalen;
     private javax.swing.JButton Updaten;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
     // End of variables declaration//GEN-END:variables
